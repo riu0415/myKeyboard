6d7596297440bfc20fea204b00743dcfa37685bd
riu0415 <144538952+riu0415@users.noreply.github.com>
2025-04-25 17:16:41 +0900

    dp

--- main.py
+++ main.py
@@ -1,71 +1,13 @@
-# A = open('log.txt').read().split()
-keys = list("@234567890]^1,.-;lrdyp[aoeiugntskf:xcvwqjhmbz/")
-keyId = {}
-for i in range(len(keys)):
-    keyId[keys[i]] = i
-hinndo = [0]*49
-# for a in A:
-#     try:
-#         hinndo[keyId[a]] += 1
-#     except:
-#         pass
-
-data = {"<Return>": 809,
-"a": 805,
-"<Backspace>": 730,
-"i": 609,
-"o": 604,
-"n": 565,
-"<Space>": 547,
-"t": 506,
-"u": 476,
-"e": 428,
-"k": 383,
-"<ShiftLeft>": 368,
-"<F>": 323,
-"<H>": 321,
-"r": 316,
-"s": 306}
-for a in data:
-    try:
-        hinndo[keyId[a]] = data[a]
-    except:
-        pass
-
-use = set()
-rank = []
-for k in keys:
-    use.add(k)
-for i in range(49):
-    Max = 0
-    key = ""
-    for a in use:
-        if Max <= hinndo[keyId[a]]:
-            key = a
-    use.discard(key)
-    rank.append(key)
-keyboard = [
-    [[],[],4],
-    [[],[],4],
-    [[],[],4],
-    [[],[],8],
-    [[],[],8],
-    [[],[],4],
-    [[],[],4],
-    [[],[],12]
-    ]
-count = 0
-for a in rank:
-    m = 10**100
-    index = -1
-    for i in range(8):
-        if sum(keyboard[i][0]) < m and len(keyboard[i][0])<keyboard[i][2]:
-            index = i
-            m = sum(keyboard[i][0])
-    try:
-        keyboard[index][0].append(hinndo[keyId[a]])
-        keyboard[index][1].append(a)
-    except:
-        pass
-for k in keyboard:
-    print(k[1])
+N,W = map(int,input().split())
+dp = [[0]*(10**5+1) for i in range(N+1)]
+Q = [list(map(int,input().split()))]
+for i in range(1,N+1):
+    w,v = map(int,input().split())
+    for j in range(10**5+1):
+        if j-v >= 0 and dp[i][j-v]-w < W:
+            dp[i][j] = max(dp[i-1][j],dp[i][j-v]+w)
+ans = 0
+for i in range(10**5+1):
+    if dp[-1][i]:
+        ans = i
+print(ans)
